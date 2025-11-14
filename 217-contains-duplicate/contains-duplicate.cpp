9daff8a4cc77ab783b05e983b81c99a189fb7d9e
Mohamed ED Deryouch <88974539+simoderyouch@users.noreply.github.com>
2025-11-14 20:09:05 +0100

    Time: 19 ms (93.26%) | Memory: 70.9 MB (95.46%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -2,10 +2,8 @@
 class Solution {
 public:
     bool containsDuplicate(std::vector<int>& nums) {
-        // Complexité temporelle O(N log N)
         std::sort(nums.begin(), nums.end()); 
 
-        // Complexité spatiale O(1) supplémentaire (si on ignore l'espace de la pile de tri)
         for (size_t i = 0; i + 1 < nums.size(); ++i) {
             if (nums[i] == nums[i+1]) {
                 return true;
