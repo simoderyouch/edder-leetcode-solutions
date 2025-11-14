fb4c7fdafb963e6215dc1b0cef39d0a64ccc5512
Mohamed ED Deryouch <88974539+simoderyouch@users.noreply.github.com>
2025-11-14 20:04:45 +0100

    Time: 51 ms (55.44%) | Memory: 90.9 MB (30.45%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+
+        unordered_map<int, int> element_counts;
+        for (int num : nums) {
+
+            element_counts[num]++;
+            if (element_counts[num] > 1) {
+  
+            return true;
+         }
+        }
+        return false;
+    }
+};
