993658ff3dad51e15b00799e703f8439881a72d6
Mohamed ED Deryouch <88974539+simoderyouch@users.noreply.github.com>
2025-11-14 20:08:38 +0100

    Time: 11 ms (98.79%) | Memory: 71.1 MB (77.86%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,15 +1,15 @@
+
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) {
-        
+    bool containsDuplicate(std::vector<int>& nums) {
+        // Complexité temporelle O(N log N)
+        std::sort(nums.begin(), nums.end()); 
 
-        unordered_map<int, int> element_counts;
-        for (int num : nums) {
-
-            element_counts[num]++;
-            if (element_counts[num] > 1) {
-               return true;
-         }
+        // Complexité spatiale O(1) supplémentaire (si on ignore l'espace de la pile de tri)
+        for (size_t i = 0; i + 1 < nums.size(); ++i) {
+            if (nums[i] == nums[i+1]) {
+                return true;
+            }
         }
         return false;
     }
