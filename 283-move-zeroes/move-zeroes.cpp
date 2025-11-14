b6efd3b327b467c6258ddfaa41e435ae20d9c38b
Mohamed ED Deryouch <88974539+simoderyouch@users.noreply.github.com>
2025-11-14 19:11:03 +0100

    Time: 0 ms (100.00%) | Memory: 23.7 MB (82.12%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        
+        int n = nums.size();
+        int i = 0; 
+        
+        int j = 0; 
+        
+        while( j <  n) {
+            
+            if(nums[j] != 0) {
+                nums[i] = nums[j]; 
+                i++;               
+            }
+            
+            j++;
+        }
+
+        while (i < n) {
+            nums[i] = 0; 
+            i++;         
+        }
+    }
+};
