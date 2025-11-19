49b099fa1ebd1690a6561a9ac926bb982b578f3b
Mohamed ED Deryouch <88974539+simoderyouch@users.noreply.github.com>
2025-11-19 16:47:24 +0100

    Time: 4 ms (30.14%) | Memory: 8.5 MB (32.67%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
         string ans = "";
-        int i = 0, j = 0;
+        int i = 0;
         
-        while (i < word1.size() || j < word2.size()) {
-            if (i < word1.size()) ans += word1[i++];
-            if (j < word2.size()) ans += word2[j++];
+        while (i < word1.size() || i < word2.size()) {
+            if (i < word1.size()) ans += word1[i];
+            if (i < word2.size()) ans += word2[i];
+            i++;
         }
         
         return ans;
